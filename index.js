f001b48180c15d43e4c0d39082c48a12579349ec
haoruikun <ruikunhao@Ruikuns-iMac.local>
2021-02-21 19:48:38 -0800

    assignment complete

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
     setTimeout(function(){
+        $('body').css('position','static');
         $('#preloader').fadeOut();
     }, 1500)
+})
+
+$(document).ready(function(){
+    setTimeout(function(){
+        $('.banner').css('opacity', '1');
+    }, 2500)
 })
